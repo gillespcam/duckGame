2fac89c2a3c7db63a2a9ca6b3512a8c146ac6d18
Ronan Lambert <rflambert@live.co.uk>
2019-09-26 18:36:21 +1200

    Added base activities and layouts

---
+++ app/src/main/java/com/example/duckgame/ScoresActivity.java
@@ -0,0 +1,14 @@
+package com.example.duckgame;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scores);
+    }
+}
